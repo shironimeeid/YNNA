302790197bc2d1eaf8bc1bf9583a4b58a2d1a6f0
Shironee.XRC <shirokochan1030@gmail.com>
2024-02-12 00:06:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1820,7 +1820,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         if (events.length === 0) {
-            eventsContainer.innerHTML = '<p>Event coming soon...</p>';
+            eventsContainer.innerHTML = '<p>Event yang kamu cari tidak ada.</p>';
         }
     }
 
